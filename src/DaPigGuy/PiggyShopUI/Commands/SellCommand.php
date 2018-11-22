bff1f6c91259f231c08390733fca3b4d669aae66
Aericio <aericiox@gmail.com>
2018-11-21 23:38:11 -1000

    Compliance with C1a, B3 + v1.0.1
    * refactor cause screw you pig
    * how the fuck to virion?

--- src/DaPigGuy/PiggyShopUI/Commands/SellCommand.php
+++ src/DaPigGuy/PiggyShopUI/Commands/SellCommand.php
@@ -1,37 +1,37 @@
 <?php
 
-namespace PiggyShopUI\Commands;
+namespace DaPigGuy\PiggyShopUI\Commands;
 
+use DaPigGuy\PiggyShopUI\Main;
 use jojoe77777\FormAPI\FormAPI;
-use PiggyShopUI\Main;
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 
 /**
- * Class BuyCommand
- * @package PiggyShopUI\Commands
+ * Class SellCommand
+ * @package DaPigGuy\PiggyShopUI\Commands
  */
-class BuyCommand extends PluginCommand
+class SellCommand extends PluginCommand
 {
     /**
-     * BuyCommand constructor.
+     * SellCommand constructor.
      * @param string $name
-     * @param Main $plugin
+     * @param Main   $plugin
      */
     public function __construct(string $name, Main $plugin)
     {
         parent::__construct($name, $plugin);
-        $this->setDescription("Access the shop UI");
-        $this->setPermission("piggyshopui.command.buy");
-        $this->setUsage("/buy [category]");
+        $this->setDescription("Access the sell UI");
+        $this->setPermission("piggyshopui.command.sell");
+        $this->setUsage("/sell [category]");
     }
 
     /**
      * @param CommandSender $sender
-     * @param string $commandLabel
-     * @param array $args
+     * @param string        $commandLabel
+     * @param array         $args
      * @return bool|mixed|void
      */
     public function execute(CommandSender $sender, string $commandLabel, array $args)
@@ -42,14 +42,14 @@ class BuyCommand extends PluginCommand
             if ($formapi instanceof FormAPI && $formapi->isEnabled()) {
                 if ($sender instanceof Player) {
                     if (isset($args[0])) {
-                        if (isset($plugin->buyCategories[$args[0]])) {
-                            $plugin->openBuyCategoryMenu($sender, $args[0]);
+                        if (isset($plugin->sellCategories[$args[0]])) {
+                            $plugin->openSellCategoryMenu($sender, $args[0]);
                             return;
                         }
                         $sender->sendMessage(TextFormat::RED . "Invalid category.");
                         return;
                     }
-                    $plugin->openBuyMainMenu($sender);
+                    $plugin->openSellMainMenu($sender);
                     return;
                 }
                 $sender->sendMessage(TextFormat::RED . "Please use this in-game.");

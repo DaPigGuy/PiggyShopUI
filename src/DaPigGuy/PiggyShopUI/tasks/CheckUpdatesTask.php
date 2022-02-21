adc9e31e02f07cd3468c4d63b0da3ec5cc82b09b
DaPigGuy <mcpepig123@gmail.com>
2022-02-21 15:59:46 -0800

    Updated auto updater

--- src/DaPigGuy/PiggyShopUI/tasks/CheckUpdatesTask.php
+++ src/DaPigGuy/PiggyShopUI/tasks/CheckUpdatesTask.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace DaPigGuy\PiggyShopUI\tasks;
 
 use DaPigGuy\PiggyShopUI\PiggyShopUI;
-use Exception;
 use pocketmine\plugin\ApiVersion;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\utils\Internet;
@@ -14,29 +13,24 @@ class CheckUpdatesTask extends AsyncTask
 {
     public function onRun(): void
     {
-        $this->setResult([Internet::getURL("https://poggit.pmmp.io/releases.json?name=PiggyShopUI", 10, [], $error), $error]);
+        $result = Internet::getURL("https://poggit.pmmp.io/releases.json?name=PiggyShopUI", 10, [], $error);
+        $this->setResult([$result?->getBody(), $error]);
     }
 
     public function onCompletion(): void
     {
         $plugin = PiggyShopUI::getInstance();
-        try {
-            if ($plugin->isEnabled()) {
-                $results = $this->getResult();
-
-                $error = $results[1];
-                if ($error !== null) throw new Exception($error);
-
-                $data = json_decode($results[0]->getBody(), true);
-                if (version_compare($plugin->getDescription()->getVersion(), $data[0]["version"]) === -1) {
-                    if (ApiVersion::isCompatible($plugin->getServer()->getApiVersion(), $data[0]["api"][0])) {
-                        $plugin->getLogger()->info("PiggyShopUI v" . $data[0]["version"] . " is available for download at " . $data[0]["artifact_url"] . "/PiggyShopUI.phar");
-                    }
+        [$body, $error] = $this->getResult();
+        if ($error) {
+            $plugin->getLogger()->warning("Auto-update check failed.");
+            $plugin->getLogger()->debug($error);
+        } else if ($plugin->isEnabled()) {
+            $data = json_decode($body, true);
+            if (version_compare($plugin->getDescription()->getVersion(), $data[0]["version"]) === -1) {
+                if (ApiVersion::isCompatible($plugin->getServer()->getApiVersion(), $data[0]["api"][0])) {
+                    $plugin->getLogger()->info("PiggyShopUI v" . $data[0]["version"] . " is available for download at " . $data[0]["artifact_url"] . "/PiggyShopUI.phar");
                 }
             }
-        } catch (Exception $exception) {
-            $plugin->getLogger()->warning("Auto-update check failed.");
-            $plugin->getLogger()->debug((string)$exception);
         }
     }
 }
